ca30fd3b5d1cf5b49fe281d9a08342ee8b66d781
kamath7 <adithyakamath96@gmail.com>
2021-09-13 14:23:08 +0530

    initial commit. Day0 of basic installation and using C# with unity done

---
+++ Day0-Basics/Day0Project/Assets/HelloWorld.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HelloWorld : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        print("Hello Kams!");
+        print("Something good is happening!");
+        print("Using VS2017!");
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
